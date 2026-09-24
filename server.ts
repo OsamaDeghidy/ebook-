93be12fa35c38630c4005975262a1dd71eee10c8
Temporary User <temp@example.com>
2026-09-24 19:50:44 +0300

    run

--- server.ts
+++ server.ts
@@ -1,3 +1,23 @@
+// Global polyfills for Node.js environments (Render / Linux / Cloud deployment)
+if (typeof (globalThis as any).DOMMatrix === 'undefined') {
+  class DOMMatrixMock {
+    a = 1; b = 0; c = 0; d = 1; e = 0; f = 0;
+    m11 = 1; m12 = 0; m13 = 0; m14 = 0;
+    m21 = 0; m22 = 1; m23 = 0; m24 = 0;
+    m31 = 0; m32 = 0; m33 = 1; m34 = 0;
+    m41 = 0; m42 = 0; m43 = 0; m44 = 1;
+    is2D = true;
+    isIdentity = true;
+  }
+  (globalThis as any).DOMMatrix = DOMMatrixMock;
+}
+if (typeof (globalThis as any).ImageData === 'undefined') {
+  (globalThis as any).ImageData = class ImageDataMock {};
+}
+if (typeof (globalThis as any).Path2D === 'undefined') {
+  (globalThis as any).Path2D = class Path2DMock {};
+}
+
 import express from "express";
 import path from "path";
 import fs from "fs";
